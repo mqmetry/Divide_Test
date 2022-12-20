f49a0959e74a901019f70d95970b4189c9d8a3a7
mqmetry <119501023+mqmetry@users.noreply.github.com>
2022-12-20 14:45:24 +0530

    Update FizzBuzz.java

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -5,8 +5,9 @@ public class FizzBuzz {
     public String play(int number) {
 
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
+        if (number % 2 == 0) return "2";
+        if (number % 3 == 0) return "3";
+        if (number % 5 == 0) return "5";
 
 
         return String.valueOf(number);
